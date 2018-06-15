89854c46c49dfc76b5d7330dae7b67fef92e8a84
TENMYO Masakazu <tenmyo@gmail.com>
2018-06-15 12:34:48 +0900

    WIP

--- run.sh
+++ run.sh
@@ -2,6 +2,6 @@
 set -eu
 clear
 cmake --build build/
-/usr/share/clang/run-clang-tidy.py -p ./build -fix -format
+/usr/share/clang/run-clang-tidy.py -p ./build -fix -format -header-filter=.\*
 #valgrind --leak-check=full --track-origins=yes --show-reachable=yes --suppressions=suppressions.txt --gen-suppressions=yes \
 /usr/bin/time  ./build/bin/optcarrot_cpp
