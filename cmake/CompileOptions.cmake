c0161bbbee9222bd93b1f84616e6128d50225bfc
TENMYO Masakazu <tenmyo@gmail.com>
2018-06-16 00:55:53 +0900

    WIP: lint conf

--- cmake/CompileOptions.cmake
+++ cmake/CompileOptions.cmake
@@ -16,6 +16,7 @@ if(CMAKE_C_COMPILER_ID MATCHES "Clang")
     -Wno-c++98-compat
     -Wno-c++98-compat-pedantic
     -Wno-padded
+    -Wno-weak-vtables
     )
 elseif(CMAKE_C_COMPILER_ID MATCHES "GNU")
   set(DEFAULT_COMPILE_OPTIONS ${DEFAULT_COMPILE_OPTIONS}
@@ -24,15 +25,21 @@ elseif(CMAKE_C_COMPILER_ID MATCHES "GNU")
     -Wextra
     -Wcast-align
     -Wcast-qual
+    -Wctor-dtor-privacy
     -Wdisabled-optimization
     -Wformat=2
     -Winit-self
     -Wlogical-op
     -Wmissing-declarations
     -Wmissing-include-dirs
+    -Wnoexcept
+    -Wold-style-cast
+    -Woverloaded-virtual
     -Wredundant-decls
     -Wshadow
     -Wsign-conversion
+    -Wsign-promo
+    -Wstrict-null-sentinel
     -Wstrict-overflow=5
     -Wswitch-default
     -Wundef
