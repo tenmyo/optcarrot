ebd201a448bc4c113785917a29079201478a6edb
TENMYO Masakazu <tenmyo@gmail.com>
2018-06-25 16:05:50 +0900

    WIP: optcarrot::file_read to using filesystem

--- source/optcarrot.cpp
+++ source/optcarrot.cpp
@@ -9,25 +9,24 @@
 // External headers
 
 // System headers
+#include <filesystem>
 #include <fstream>
 #include <stdexcept>
 
 std::vector<uint8_t> optcarrot::file_read(const std::string &fname) {
+  using namespace std::literals::string_literals;
+  std::filesystem::path p(fname);
+  auto size = std::filesystem::file_size(p);
   std::ifstream ifs(fname, std::ios_base::in | std::ios::binary);
   if (!ifs) {
-    throw std::runtime_error("failed to open a file: " + fname);
+    throw std::runtime_error("failed to open a file: "s + fname);
   }
-  ifs.seekg(0, std::ios::end);
-  auto size = ifs.tellg();
-  ifs.seekg(0, std::ios::beg);
-  if ((size < 0) || !ifs) {
-    throw std::runtime_error("failed to get size a file: " + fname);
-  }
-  std::vector<uint8_t> buf(static_cast<size_t>(size));
+  std::vector<uint8_t> buf(size);
   // NOLINTNEXTLINE(cppcoreguidelines-pro-type-reinterpret-cast)
-  ifs.read(reinterpret_cast<char *>(buf.data()), size);
+  ifs.read(reinterpret_cast<char *>(buf.data()),
+           static_cast<std::streamsize>(size));
   if (!ifs.good()) {
-    throw std::runtime_error("failed to read a file: " + fname);
+    throw std::runtime_error("failed to read a file: "s + fname);
   }
   return buf;
 }
